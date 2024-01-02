a1affea0d3de0e46aee530962ab4eabe6bbce7a0
Abhinav Mahesh <amahesh6@illinois.edu>
2024-01-02 01:58:32 -0500

    Formatting fix

--- server/user_data.js
+++ server/user_data.js
@@ -1,8 +1,8 @@
 // import modules
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -13,17 +13,17 @@ app.use(express.json());
 
 // mysql connection
 const createConnection = () => {
-    return mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-    });
-}
+  return mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+};
 
 // Insert formData into MySQL user_data TABLE
 const insertValuesIntoTable = (db, formData, callback) => {
-    const selectQuery = `
+  const selectQuery = `
         SELECT COUNT(*) AS count
         FROM user_data
         WHERE
@@ -34,48 +34,51 @@ const insertValuesIntoTable = (db, formData, callback) => {
         AND \`monthly_down_payment\` = ${formData["Monthly Down Payment"]};
         `;
 
-    db.query(selectQuery, (selectError, results) => {
-        if (selectError) {
-            console.error('Error executing SELECT query frcom user_data table:', selectError);
-            return;
-        }
+  db.query(selectQuery, (selectError, results) => {
+    if (selectError) {
+      console.error(
+        "Error executing SELECT query frcom user_data table:",
+        selectError
+      );
+      return;
+    }
 
-        const recordCount = results[0].count;
+    const recordCount = results[0].count;
 
-        // If the record doesn't exist, insert it
-        if (recordCount === 0) {
-            const insertQuery = `
+    // If the record doesn't exist, insert it
+    if (recordCount === 0) {
+      const insertQuery = `
                 INSERT INTO user_data
                 (\`annual_income\`, \`monthly_expense\`, \`credit_score\`, \`available_savings\`, \`monthly_down_payment\`)
                 VALUES
                 (${formData["Annual Income"]}, ${formData["Monthly Expenditure"]}, ${formData["Credit Score"]}, ${formData["Available Savings"]}, ${formData["Monthly Down Payment"]});
             `;
 
-            db.query(insertQuery, (insertError) => {
-                if (insertError) {
-                    console.error('Error executing INSERT query:', insertError);
-                } else {
-                    console.log('Data inserted successfully into user_data table');
-                }
-                callback(false);
-            });
+      db.query(insertQuery, (insertError) => {
+        if (insertError) {
+          console.error("Error executing INSERT query:", insertError);
         } else {
-            console.log('Data already exists in user_data table');
-
-            // Close the database connection
-            db.end();
-            callback(true);
+          console.log("Data inserted successfully into user_data table");
         }
-    })
+        callback(false);
+      });
+    } else {
+      console.log("Data already exists in user_data table");
+
+      // Close the database connection
+      db.end();
+      callback(true);
+    }
+  });
 };
 
 // Calculate risk score and insert into user_risk_score table
 const calculateRiskScore = (db, formData) => {
-    // calculate risk score logic
-    const risk_score = 0;
+  // calculate risk score logic
+  const risk_score = 0;
 
-    // insert into table with correct user_id
-    const selectQuery = `
+  // insert into table with correct user_id
+  const selectQuery = `
         SELECT user_id
         FROM user_data
         WHERE
@@ -86,64 +89,73 @@ const calculateRiskScore = (db, formData) => {
         AND \`monthly_down_payment\` = ${formData["Monthly Down Payment"]};
         `;
 
-    db.query(selectQuery, (selectError, results) => {
-        if (selectError) {
-            console.error('Error executing SELECT query from user_data to calculate user_risk_score:', selectError);
-            return;
-        }
+  db.query(selectQuery, (selectError, results) => {
+    if (selectError) {
+      console.error(
+        "Error executing SELECT query from user_data to calculate user_risk_score:",
+        selectError
+      );
+      return;
+    }
 
-        const user_id = results[0].user_id;
+    const user_id = results[0].user_id;
 
-        const insertQuery = `
+    const insertQuery = `
             INSERT INTO user_risk_score
             (\`user_id\`, \`u_risk_score\`)
             VALUES
             (${user_id}, ${risk_score});
         `;
 
-        db.query(insertQuery, (insertError) => {
-            if (insertError) {
-                console.error('Error executing INSERT query into user_risk_score table:', insertError);
-            } else {
-                console.log('Data inserted successfully into user_risk_score table');
-            }
-            // Close the database connection
-            db.end();
-            console.log('Database connection is closed after calculating risk score.');
-        });
+    db.query(insertQuery, (insertError) => {
+      if (insertError) {
+        console.error(
+          "Error executing INSERT query into user_risk_score table:",
+          insertError
+        );
+      } else {
+        console.log("Data inserted successfully into user_risk_score table");
+      }
+      // Close the database connection
+      db.end();
+      console.log(
+        "Database connection is closed after calculating risk score."
+      );
     });
+  });
 };
 
 // Put request to recieve data from frontend and insert into tables
 app.put("/", (req, res) => {
-    const formData = req.body;
-    
-    console.log('Received formData on the server', formData);
-    res.send("Data received successfully!");
+  const formData = req.body;
 
-    // Create db connection
-    const db = createConnection();
+  console.log("Received formData on the server", formData);
+  res.send("Data received successfully!");
 
-    // check if connection is successful
-    db.connect((err) => {
-        if (err) {
-            console.error('Error connecting to MySQL:', err.message);
-        } else {
-            console.log('Connected to MySQL!');
-        }
-    });
+  // Create db connection
+  const db = createConnection();
 
-    insertValuesIntoTable(db, formData, (isDisconnected) => {
-        // Callback function after the data is inserted
-        if (!isDisconnected) {
-            calculateRiskScore(db, formData);
-        } else {
-            console.log('Database connection is closed. Cannot calculate risk score.');
-        }
-    });
-});  
+  // check if connection is successful
+  db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to MySQL:", err.message);
+    } else {
+      console.log("Connected to MySQL!");
+    }
+  });
 
-app.listen(process.env.PORT, () => {
-    console.log("Listening...");
+  insertValuesIntoTable(db, formData, (isDisconnected) => {
+    // Callback function after the data is inserted
+    if (!isDisconnected) {
+      calculateRiskScore(db, formData);
+    } else {
+      console.log(
+        "Database connection is closed. Cannot calculate risk score."
+      );
+    }
+  });
 });
 
+app.listen(process.env.PORT, () => {
+  console.log("Listening...");
+});
