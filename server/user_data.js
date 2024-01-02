91fe6ab78527d8bf840ae6260ff918aa770e6e37
Abhinav Mahesh <amahesh6@illinois.edu>
2024-01-01 21:31:55 -0500

    Database connection established

--- server/user_data.js
+++ server/user_data.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err.message);
+    } else {
+        console.log('Connected to MySQL!');
+    }
+});
+
 app.listen(5173, () => {
-    console.log("Listening...")
+    console.log("Listening...");
 });
