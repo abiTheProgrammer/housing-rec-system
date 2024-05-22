e28b34f08ad95905eb23a123bef4955a6357071d
Abhinav Mahesh <amahesh6@illinois.edu>
2024-05-22 15:49:38 -0500

    Added comment to run

--- src/data_processing/housing_data.py
+++ src/data_processing/housing_data.py
@@ -4,5 +4,6 @@ def housing_data_from_scrape():
     print("scrape housing data")
     return "house data scraped"
 
+# to run the file using "python3 housing_data.py"
 if __name__ == "__main__":
     housing_data_from_scrape()
