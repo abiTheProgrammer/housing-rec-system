147b1b09107ee2e843fbea7a08369f4fb5e6ffb7
Abhinav Mahesh <amahesh6@illinois.edu>
2024-05-25 12:31:15 -0500

    HTML output stored in file data/mls_listings_html.txt to be parsed

--- src/data_processing/housing_data.py
+++ src/data_processing/housing_data.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup
 
 def housing_data_from_scrape():
     r = requests.get("https://www.mls.com/Search/California.mvc")
-    print(r.status_code)
-    
     soup = BeautifulSoup(r.content, 'html.parser')
-    print(soup.prettify())
+    soup_content = soup.prettify()
+    # pipe the soup_html content into local data file
+    with open("data/mls_listings_html.txt", "w") as html_data_file:
+        html_data_file.write(soup_content)
     # TODO: parse the html content
 
     return "house data scraped"
