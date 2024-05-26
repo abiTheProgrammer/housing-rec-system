da7f24bfb3a4538bab22f50c0582bf60f8e4720a
Denver Soekawan <denvers2@illinois.edu>
2024-05-26 02:15:46 -0500

    first commit

--- src/data_processing/housing_data.py
+++ src/data_processing/housing_data.py
@@ -1,7 +1,11 @@
-
+import requests
+from bs4 import BeautifulSoup
 
 def housing_data_from_scrape():
-    print("scrape housing data")
+    html_text = requests.get("https://www.mls.com/Search/California.mvc").text
+    soup = BeautifulSoup(html_text, "lxml").prettify()
+    with open("mls_listings_html.txt", "w") as html_data_file:
+        html_data_file.write(soup)
     return "house data scraped"
 
 if __name__ == "__main__":
