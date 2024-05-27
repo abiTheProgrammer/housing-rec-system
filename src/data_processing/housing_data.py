be1036627014f5704c526ae892df98a88c55166a
Abhinav Mahesh <amahesh6@illinois.edu>
2024-05-27 16:45:45 -0500

    URL data to be fetched for each listing

--- src/data_processing/housing_data.py
+++ src/data_processing/housing_data.py
@@ -6,10 +6,14 @@ def housing_data_from_scrape():
     soup = BeautifulSoup(r.content, 'html.parser')
     soup_content = soup.prettify()
     # pipe the soup_html content into local data file
-    with open("data/mls_listings_html.txt", "w") as html_data_file:
+    with open("data/mls_listings_california.html", "w") as html_data_file:
         html_data_file.write(soup_content)
     # TODO: parse the html content
-
+    listings = soup.find_all("ul", class_ = "sub-section-list")
+    for ul_row in listings:
+        li_list = ul_row.find_all("li")
+        print(li_list)
+        print('\n')
     return "house data scraped"
 
 # to run the file using "python3 housing_data.py"
