fd5d91bec34cd4471b281dbf6015cf5515f5128f
Denver Soekawan <denvers2@illinois.edu>
2024-05-27 01:44:33 -0500

    added listings

--- src/data_processing/housing_data.py
+++ src/data_processing/housing_data.py
@@ -3,9 +3,14 @@ from bs4 import BeautifulSoup
 
 def housing_data_from_scrape():
     html_text = requests.get("https://www.mls.com/Search/California.mvc").text
-    soup = BeautifulSoup(html_text, "lxml").prettify()
+    # soup has type <class 'bs4.BeautifulSoup'>
+    soup = BeautifulSoup(html_text, "lxml")
+    # soup has type <class 'str'>
+    prettified = soup.prettify()
     with open("mls_listings_html.txt", "w") as html_data_file:
-        html_data_file.write(soup)
+        html_data_file.write(prettified)
+    listings = soup.find("div", class_ = "col-md-8 col-sm-7 col-xs-12")
+    print(listings)
     return "house data scraped"
 
 if __name__ == "__main__":
