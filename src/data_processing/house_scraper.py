cf3c5dd92455053c312d1372575f2853033bf68b
Abhinav Mahesh <amahesh6@illinois.edu>
2024-06-01 22:39:15 -0500

    Area content to be parsed

--- src/data_processing/house_scraper.py
+++ src/data_processing/house_scraper.py
@@ -10,23 +10,31 @@ class HouseScraper:
         soup = BeautifulSoup(r.content, 'html.parser')
         soup_content = soup.prettify()
         # pipe the soup_html content into local data file
-        with open("data/mls_listings_california.html", "w") as html_data_file:
-            html_data_file.write(soup_content)
-        # <ul class = "sub-section-list"> contains urls of each listing
+        with open("data/mls_listings_california.html", "w") as data_file:
+            data_file.write(soup_content)
+        # <ul class = "sub-section-list"> contains urls of each listings for each area
         rows = soup.find_all("ul", class_ = "sub-section-list")
         for ul_row in rows:
-            # handle regular house listings
+            # handle regular area listings
             if ul_row.a.get('class') == None:
-                listings = ul_row.find_all("a")
-                for link in listings:
-                    print(link.text)
-                    print(link.get('href'))
-                #     r = requests.get("https://www.mls.com" + link.get('href'))
-                #     print(r.content)
-                # break
+                areas = ul_row.find_all("a")
+                for link in areas:
+                    metro_area = link.text
+                    metro_area_url = link.get('href')
+                    print("Metro Area: \"" + metro_area + '\"\n' + "Path: \"" + metro_area_url + "\"")
+                    # parse the url of each area to get listings in that area
+                    r = requests.get("https://www.mls.com" + metro_area_url)
+                    soup = BeautifulSoup(r.content, 'html.parser')
+                    soup_content = soup.prettify()
+                    # pipe the area content into local data file
+                    # temp: file name is "north-coast" because only last area is saved to file (remove once content is examined)
+                    with open("data/mls_listings_california_north_coast.html", "w") as data_file:
+                        data_file.write(soup_content)
+                    # TODO: parse the content of each area page to get house data for each neighborhood
+
                 print('\n', end="")
             else:
-                # handle foreclosure listings
+                # TODO: handle foreclosure listings
                 pass
         return "house data scraped"
 
