b24d1eb38d0743f8be5462047ce38622a123739b
Denver Soekawan <denvers2@illinois.edu>
2024-06-04 03:30:22 -0500

    edited README.md

--- docs/README.md
+++ docs/README.md
@@ -1,9 +1,11 @@
 # Housing Recommendation System
 
 ## Overview
+
 The Housing Recommendation System is a Model designed to recommend Houses to a User based on the User's financial needs. Houses can additionally be filtered and rated to provide feedback to the model to further improve recommendations.
 
 ## Features
+
 **User Financial Needs**: Input user financial status and expectations such as ...
 
 **Recommendations**: House recommendations based on User needs and House features such as ...
@@ -13,10 +15,66 @@ The Housing Recommendation System is a Model designed to recommend Houses to a U
 **Recommendation Filtering**: Recommend houses filtered by the user based on preferences like house location, type, bedrooms, square feet, and more.
 
 ## Steps
-**1. Collect User and Housing Data from different Data Sources (Web Scrape/Real Estate Data/Housing Web App APIs)**
+
+**1. Collect User and Housing Data from https://mls.com/ (Web Scrape Real Estate Data)**
+
+The website contains housing data for every state in the United States. In each state, there are two or three sections. There exist a ***non-rural*** and a ***foreclosures*** section in all fifty states. However, some states also have a third ***rural*** section. A ***non-rural*** section has ***cities***, whereas a ***rural*** section has ***census-designated places (CDP)***. Each ***city*** or ***CDP*** contains listing information for houses, such as price, address, number of bedrooms and bathrooms, area size, and type of house.
+
+country (USA) -> state -> non-rural/rural/foreclosures
+
+non-rural: contains cities
+
+rural: contains census-designated places (CDP)
 
 **2. Clean and Preprocess Data**
 
 **3. EDA and Feature Engineering**
 
 **4. Build Recommendation System**
+
+## Setup
+
+**1. Clone Github Repo**:
+
+```sh
+git clone https://github.com/abiTheProgrammer/housing-rec-system.git
+cd housing-rec-system
+```
+
+**2. Create and Activate Virtual Enviornment**:
+
+```sh
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+**3. Install Dependencies**:
+
+```sh
+pip install -r requirements.txt
+```
+
+**4. Run the script**:
+
+```sh
+python3 src/data_processing/house_scraper.py
+```
+
+## EC2 Setup
+
+**1. Login into AWS console**
+
+**2. Create Amazon Linux EC2 Virtual Instance. Select free tier**
+
+**3. Create a new key pair. Download it as keypair1.pem**
+
+**4. Connect to your EC2 instance using the pem file.**
+
+**5. Install git using this command:**
+
+```sh
+   sudo yum update -y
+   sudo yum install git -y
+   git config --global user.name “Your Name”
+   git config --global user.email “your_email@example.com”
+```
--- src/data_processing/housing_data.py
+++ src/data_processing/housing_data.py
@@ -1,17 +1,84 @@
 import requests
 from bs4 import BeautifulSoup
 
-def housing_data_from_scrape():
-    html_text = requests.get("https://www.mls.com/Search/California.mvc").text
-    # soup has type <class 'bs4.BeautifulSoup'>
-    soup = BeautifulSoup(html_text, "lxml")
-    # soup has type <class 'str'>
-    prettified = soup.prettify()
-    with open("mls_listings_html.txt", "w") as html_data_file:
-        html_data_file.write(prettified)
-    listings = soup.find("div", class_ = "col-md-8 col-sm-7 col-xs-12")
-    print(listings)
-    return "house data scraped"
+# def housing_data_from_scrape():
+#     html_text = requests.get("https://www.mls.com/Search/California.mvc").text
+#     # soup has type <class 'bs4.BeautifulSoup'>
+#     soup = BeautifulSoup(html_text, "lxml")
+#     # soup has type <class 'str'>
+#     prettified = soup.prettify()
+#     with open("mls_listings_html.txt", "w") as html_data_file:
+#         html_data_file.write(prettified)
+#     listings = soup.find("div", class_ = "col-md-8 col-sm-7 col-xs-12")
+#     print(listings)
+#     # listings = soup.find_all("ul", class_ = "sub-section-list")
+#     # for ul_row in listings:
+#     #     li_list = ul_row.find_all("li")
+#     #     print(li_list)
+#     #     print('\n')
+#     return "house data scraped"
 
+class HouseScraper:
+    def __init__(self) -> None:
+        pass
+
+    def scrape_state(self, state: str):
+        # parse html content of main html page
+        r = requests.get(f"https://www.mls.com/Search/{state}.mvc").text
+        soup = BeautifulSoup(r, "lxml")
+        # # pipe the soup_html content into local data file (remove once analysis of html content completed)
+        prettified = soup.prettify()
+        with open(f"data/mls_listings_{state.lower()}.html", "w") as data_file:
+            data_file.write(prettified)
+        # <ul class = "sub-section-list"> contains urls of each listings for each area
+        rows = soup.find_all("ul", class_ = "sub-section-list")
+        print(rows[2])
+        print(rows[2].a.get("class"))
+        # for ul_row in rows:
+        #     # handle regular area listings
+        #     if ul_row.a.get('class') == None:
+        #         areas = ul_row.find_all("a")
+        #         for area in areas:
+        #             # for each area: parse
+        #             HouseScraper.scrape_area(self, area)
+        #             # include break statement to analyze
+        #             break
+        #         print('\n', end="")
+        #     else:
+        #         # TODO: handle foreclosure listings
+        #         pass
+        #     # include break statement to analyze
+        #     break
+    
+    def scrape_area(self, area: str):
+        metro_area = area.text
+        metro_area_url = area.get('href')
+        print("Metro Area: \"" + metro_area + '\"\n' + "Path: \"" + metro_area_url + "\"", end='\n\n')
+        # parse the url of area to get listings in that area
+        r = requests.get("https://www.mls.com" + metro_area_url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        # pipe the area content into local data file (remove once analysis of html content completed)
+        soup_content = soup.prettify()
+        # temp: file name is "bakersfield" because only this area is saved to file (remove once content is examined)
+        with open("mls_listings_california_bakersfield.html", "w") as data_file:
+            data_file.write(soup_content)
+        # TODO: parse the content of each area page to get house data for each neighborhood
+        rows = soup.find_all("ul", class_ = "sub-section-list")
+        for ul_row in rows:
+            # handle only listings that do not have the title "Foreclosure"
+            if 'Foreclosure' not in ul_row.a.text:
+                home_links = ul_row.find_all("a")
+                for link in home_links:
+                    # for each home_link: parse
+                    neighborhood = link.text
+                    neighborhood_link = link.get('href')
+                    print("Neighborhood: \"" + neighborhood + '\"\n' + "Path: \"" + neighborhood_link + "\"", end='\n\n')
+
+# if __name__ == "__main__":
+#     housing_data_from_scrape()
+
+# to run the file using "python3 <relative-path-of-file>"
 if __name__ == "__main__":
-    housing_data_from_scrape()
+    h = HouseScraper()
+    # TODO: update with a list of states
+    h.scrape_state("California")
